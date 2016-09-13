52ea92d2c045cd74944d6124f5d9cf790027ee98
Becca Ricks <baricks@gmail.com>
2016-09-13 09:17:07 -0400

    updated styles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
           <h3>Programming A to Z, Week 1</h3>
         </div>
         <div className="App-intro">
+          <span><a href="https://en.wikipedia.org/wiki/Finnegans_Wake">Finnegans Wake</a> is a novel by James Joyce written in an experimental stream of consciousness style that rejects narrative conventions. This tool allows you to generate a text that mimics both the style and the vocabulary of the novel.</span><br/><br/>
           <span>Write something or paste text in the box below. You can also upload a text file.</span><br/><br/>
           <Formy/>
         </div>
